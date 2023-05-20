8577f365c1913d9c0cf3b50925fbe180991cdd84
Hery Sehastian <sehastian.hery@gmail.com>
2023-05-20 22:25:50 +0700

    learn lume setting

--- _config.ts
+++ _config.ts
@@ -1,4 +1,8 @@
 import lume from "lume/mod.ts";
+import jsx from "lume/plugins/jsx.ts";
+import tailwindcss from "lume/plugins/tailwindcss.ts";
+import postcss from "lume/plugins/postcss.ts";
+import minifyHTML from "lume/plugins/minify_html.ts";
 
 const site = lume({
     src: "./src",
@@ -6,5 +10,14 @@ const site = lume({
 });
 
 site.ignore("README.md");
+
+site.use(jsx())
+    .use(tailwindcss({
+        extensions: [".html", ".jsx"]
+    }))
+    .use(postcss())
+    .use(minifyHTML());
+
+site.copy("favicon.ico");
 
 export default site;
